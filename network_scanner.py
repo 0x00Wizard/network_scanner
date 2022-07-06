210b32f6e53acdec3dfbe0530e78cf9e4390311f
wizardxxdx <101555271+wizardxxdx@users.noreply.github.com>
2022-07-06 05:05:10 -0700

    update the code

--- network_scanner.py
+++ network_scanner.py
@@ -7,15 +7,21 @@ def scan(ip):
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast / arp_request
     answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
-    print("IP \t\t\tMAC Address \n------------------------------------")
+
     client_list = []
 
     for data in answered_list:
         client_dict = {"ip": data[1].psrc, "mac": data[1].hwsrc}
         client_list.append(client_dict)
-        print(f"{data[1].psrc} \t\t {data[1].hwsrc}")
-        print("-" * 30)
-    print(client_list)
+        # print(f"{data[1].psrc} \t\t {data[1].hwsrc}")
+        # print("-" * 30)
+    return client_list
+
+
+def print_results(results_list):
+    print("IP \t\t\tMAC Address \n------------------------------------")
+    for client in results_list:
+        print(client)
 
 
 scan("192.168.3.1/24")
