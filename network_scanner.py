58e76b846d2bcb0932179caa8abbab94f015c33c
wizardxxdx <101555271+wizardxxdx@users.noreply.github.com>
2022-07-05 23:04:48 -0700

    update the code

--- network_scanner.py
+++ network_scanner.py
@@ -3,7 +3,9 @@ import scapy.all as scapy
 
 
 def scan(ip):
-    scapy.arping(ip)
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    scapy.ls(broadcast)
 
 
-scan("192.168.0.103")
+scan("192.168.0.1")
