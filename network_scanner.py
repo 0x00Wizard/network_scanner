e6d9f3c6e06fc0e2190fcc9e4eb0856bd06537ae
wizardxxdx <101555271+wizardxxdx@users.noreply.github.com>
2022-07-06 00:17:24 -0700

    update the code

--- network_scanner.py
+++ network_scanner.py
@@ -8,6 +8,8 @@ def scan(ip):
     arp_request_broadcast = broadcast / arp_request
     answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
     print("IP \t\t\tMAC Address \n------------------------------------")
+    client_list = []
+
     for data in answered_list:
         print(f"{data[1].psrc} \t\t {data[1].hwsrc}")
         print("-" * 30)
